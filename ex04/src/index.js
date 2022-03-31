eac396f7f26663ce7c6020c1847aefaca5d2b743
AnelB <anel.b@live.com>
2022-03-31 17:21:48 +0200

    cetvrto ubacivanje

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){	
+    // Only change code below this line
+    var myDecimal = 5.8;
+    var mydecimalE = 2.54e-3;
+    // Only change code above this line
+    return {
+    myDecimal,
+    mydecimalE
+    };
+}
+
+console.log(main());	
+module.experts = main;
