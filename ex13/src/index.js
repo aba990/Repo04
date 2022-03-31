2138248b2885e48c49debf8ef637080ab6084610
AnelB <anel.b@live.com>
2022-03-31 18:27:15 +0200

    ispravka trinaesto

--- ex13/src/index.js
+++
@@ -1,7 +0,0 @@
-<h2>CatPhotoApp </h2>
-<main>
-<img src="https://www.pexels.com/photo/gray-cat-on-tree-branch-1693441/"
-	 alt="Gray Cat on Tree Branch">
-<p>Purr jump eat the grass rip the couch</p>
-<p>Purr jump eat the grass rip the couch</p>
-</main>
